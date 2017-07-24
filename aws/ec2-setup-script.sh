17b69168fc62e255adf25e290b812731b97e09f3
Sunghwan Park <shoark7@gmail.com>
2017-07-24 15:24:50 +0900

    Fix error

--- aws/ec2-setup-script.sh
+++ aws/ec2-setup-script.sh
@@ -1,1 +1,39 @@
-/home/sunghwanpark/bin/aws-scripts/ec2-setup-script.sh
+#!/bin/sh
+
+# Initialization
+clear
+echo "Initialize a instance"
+sudo apt-get update -q --yes --force-yes
+sudo apt-get upgrade -q --yes --force-yes
+sudo locale-gen ko_KR.UTF-8
+echo '\nexport LANG=ko_KR.UTF-8\nexport LC_ALL=C' >> ~/.bashrc
+echo 'alias python=python3' >> ~/.bashrc
+echo 'alias pip=pip3' >> ~/.bashrc
+sudo ln -sf /usr/share/zoneinfo/Asia/Seoul /etc/localtime
+
+
+
+echo "Initialization ended"
+
+
+# Installation
+clear
+echo "Install necessary programs like nginx, pip, etc"
+sudo apt-get install -q --yes --force-yes nginx
+sudo apt-get install -q --yes --force-yes python3-pip
+pip3 install -q uwsgi django virtualenv
+
+
+echo "Installing has ended"
+
+# Customization part
+## vim setting
+clear
+echo "Customization like vim setting start"
+curl https://raw.githubusercontent.com/shoark7/Daily-practice/master/others/.vimrc > ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim -c VundleUpdate -c quitall
+
+
+source ~/.bashrc
+echo "Customization ended"
